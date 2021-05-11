aee0e7c67deb4d90cab3d789d4151d87c045829f
Mark9Mbugua <mbuguamark@gmail.com>
2021-05-11 15:14:28 +0300

    Implemented the upload of images into a local directory functionality

--- routes/articles.js
+++ routes/articles.js
@@ -1,19 +1,34 @@
 const router = require('express').Router();
+const multer = require('multer');
+const path = require('path');
 let Article = require('../models/article.model');
 
-router.route('/').get((req, res) => {
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, path.join(__dirname, '../uploads/'));
+    },
+    filename: (req, file, callback) => {
+        callback(null, file.originalname);
+    },
+});
+
+const upload = multer({storage: storage});
+
+router.get('/', (req, res) => {
     Article.find()
         .then(articles => res.status(200).json(articles))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/create').post((req, res) => {
+router.post('/create', upload.single('image'), (req, res) => {
     const title = req.body.title;
     const body = req.body.body;
+    const image = req.file.originalname;
 
     const newArticle = new Article({
         title, 
-        body, 
+        body,
+        image, 
     });
 
     newArticle.save()
